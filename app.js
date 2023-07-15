3a946ac2b5361bcaaace961bb917498287da44f9
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-07-15 20:47:15 +1000

    Added CORS - Added CORS - Updated some routes to return arrays of items not objects

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require("cors")
 
-var messageCreateRouter = require('./routes/vaultItem/create');
-var messageReadRouter = require('./routes/vaultItem/read');
-var messageUpdateRouter = require('./routes/vaultItem/update');
-var messageDeleteRouter = require('./routes/vaultItem/delete');
+const messageCreateRouter = require('./routes/vaultItem/create');
+const messageReadRouter = require('./routes/vaultItem/read');
+const messageUpdateRouter = require('./routes/vaultItem/update');
+const messageDeleteRouter = require('./routes/vaultItem/delete');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/api/vault/',
     messageCreateRouter,
